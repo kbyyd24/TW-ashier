3fd00e7799f7c56a28eaa87323eb8812c176c697
gaoyx <melo@gaoyuexiang.cn>
2016-05-21 21:00:53 +0800

    add openShop() method

--- src/main/java/cn/gaoyuexiang/shop/Shop.java
+++ src/main/java/cn/gaoyuexiang/shop/Shop.java
@@ -2,18 +2,32 @@ package cn.gaoyuexiang.shop;
 
 import cn.gaoyuexiang.entry.Goods;
 import cn.gaoyuexiang.entry.PreMessager;
+import cn.gaoyuexiang.reader.GoodsReader;
+import cn.gaoyuexiang.reader.PreMessagerReader;
+import cn.gaoyuexiang.reader.Reader;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
  * Created by melo on 2016/05/21.
  * Project : TW-ashier
- * 商店类，可以通过spring注入，以保存商品和优惠信息
  */
 public class Shop {
     private ArrayList<Goods> goodses;
 
     private ArrayList<PreMessager> preMessagers;
+
+    public void openShop(String goodsInfo, String preGoods) throws IOException {
+        Reader reader = new GoodsReader(goodsInfo);
+        goodses = reader.read(new Goods());
+        reader = new PreMessagerReader(preGoods);
+        preMessagers = reader.read(new PreMessager());
+    }
+
+    public void openShop() throws IOException {
+        this.openShop(GoodsReader.DEF_FILE, PreMessagerReader.DEF_FILE);
+    }
 
     public ArrayList<Goods> getGoodses() {
         return goodses;
