7af52d42819fff912993364cf2805061184d7ba0
gaoyx <melo@gaoyuexiang.cn>
2016-05-27 16:24:15 +0800

    修改测试用的购物车数组，以满足所有购物情况

--- src/test/java/cn/gaoyuexiang/ashier/SecondHalfAshierTest.java
+++ src/test/java/cn/gaoyuexiang/ashier/SecondHalfAshierTest.java
@@ -1,18 +1,26 @@
 package cn.gaoyuexiang.ashier;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
 
 /**
  * Created by melo on 16-5-27.
  */
 public class SecondHalfAshierTest {
+
+    private Ashier ashier;
+
+    @Before
+    public void initAshier() throws IOException {
+        ashier = new SecondHalfAshier();
+        ashier.loadLists();
+    }
+
     @Test
     public void checkout() throws Exception {
-        Ashier ashier = new SecondHalfAshier();
-        ashier.loadLists();
-        String[] cart = new String[]{"ITEM000000", "ITEM000000", "ITEM000002-2"};
+        String[] cart = new String[]{"ITEM000000", "ITEM000000", "ITEM000002-2", "ITEM000001-3", "ITEM000003"};
         String ticket = ashier.checkout(cart);
         System.out.println(ticket);
     }
